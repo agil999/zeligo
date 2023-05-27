45b96c7e8cd7d34efb61a5b4e5d4f8a1171f0fcf
SlimBook 14 R5 <agilpinatar@gmail.com>
2023-05-28 05:59:19 +0700

    h

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -354,7 +354,7 @@ body{
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+          <span class="visually-hidden">Previousss</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
